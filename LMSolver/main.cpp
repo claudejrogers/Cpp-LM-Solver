d94daea803b7ce862941b773643074b95103b112
Claude Rogers <claude.j.rogers@gmail.com>
2011-07-08 13:48:08 -0700

    Initial Commit

---
+++ LMSolver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LMSolver
+//
+//  Created by Claude Rogers on 7/8/11.
+//  Copyright 2011 California Institute of Technology. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
